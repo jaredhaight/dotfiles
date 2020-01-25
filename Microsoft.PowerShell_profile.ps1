379e1379dafda61d5964569ee4602800f67a3f88
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2020-01-25 10:39:46 -0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,58 +1,53 @@
-function Test-Administrator
-{  
-   $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-   $role = [Security.Principal.WindowsBuiltinRole]::Administrator
-   (New-Object Security.Principal.WindowsPrincipal $user).IsInRole($role)  
+function Test-Administrator {  
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  $role = [Security.Principal.WindowsBuiltinRole]::Administrator
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole($role)  
 }
 
 # From: https://adsecurity.org/?p=478
-function ConvertTo-Base64
-{
-    param(
-        [Parameter(Mandatory=$True)]
-        [string]$String
-    )
-    $Bytes = [System.Text.Encoding]::Unicode.GetBytes($String)
-    $EncodedText = [Convert]::ToBase64String($Bytes)
-    return $EncodedText
+function ConvertTo-Base64 {
+  param(
+    [Parameter(Mandatory = $True)]
+    [string]$String
+  )
+  $Bytes = [System.Text.Encoding]::Unicode.GetBytes($String)
+  $EncodedText = [Convert]::ToBase64String($Bytes)
+  return $EncodedText
 }
 
-function ConvertFrom-Base64
-{
-    param(
-        [Parameter(Mandatory=$True)]
-        [string]$String
-    )
-    $DecodedText = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($String))
-    $DecodedText
+function ConvertFrom-Base64 {
+  param(
+    [Parameter(Mandatory = $True)]
+    [string]$String
+  )
+  $DecodedText = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($String))
+  $DecodedText
 }
 
 # From: https://blogs.msdn.microsoft.com/luc/2011/01/21/powershell-getting-the-hash-value-for-a-string/
-function Get-StringHash
-{
-    param(
-        [string]$String,
-        [string]$Hash="SHA256"
-    )
+function Get-StringHash {
+  param(
+    [string]$String,
+    [string]$Hash = "SHA256"
+  )
 
-    $hasher = switch ($Hash){
-        "SHA512" {new-object System.Security.Cryptography.SHA512Managed}
-        "SHA256" {new-object System.Security.Cryptography.SHA256Managed}
-        "SHA1" {new-object System.Security.Cryptography.SHA1Managed}
-        "MD5" {new-object System.Security.Cryptography.MD5CryptoServiceProvider}
-    }
+  $hasher = switch ($Hash) {
+    "SHA512" { new-object System.Security.Cryptography.SHA512Managed }
+    "SHA256" { new-object System.Security.Cryptography.SHA256Managed }
+    "SHA1" { new-object System.Security.Cryptography.SHA1Managed }
+    "MD5" { new-object System.Security.Cryptography.MD5CryptoServiceProvider }
+  }
 
-    $toHash = [System.Text.Encoding]::UTF8.GetBytes($string)
-    $hashByteArray = $hasher.ComputeHash($toHash)
-    foreach($byte in $hashByteArray)
-    {
-        $res += $byte.ToString("x2")
-    }
-    return $res;
+  $toHash = [System.Text.Encoding]::UTF8.GetBytes($string)
+  $hashByteArray = $hasher.ComputeHash($toHash)
+  foreach ($byte in $hashByteArray) {
+    $res += $byte.ToString("x2")
+  }
+  return $res;
 }
 
 function Get-ExternalIPAddress {
-  return (New-Object Net.WebClient).DownloadString('http://ifconfig.io/ip').Replace("`n","")
+  return (New-Object Net.WebClient).DownloadString('http://ifconfig.io/ip').Replace("`n", "")
 }
 
 function Split-String {
@@ -88,6 +83,20 @@ function Split-String {
     $index += $substringSize
   }
 }
+
+function Test-GitRepo {
+  $pwd = Get-Location
+  $isGitDir = Test-Path $(Join-Path $pwd ".git")
+  $parentDir = (Get-Item $pwd).Parent
+  while ((![string]::IsNullOrEmpty($parentDir.ToString()) -and ($isGitDir -eq $false))) {
+    $gitPath = Join-Path $parentDir.FullName ".git"
+    $isGitDir = Test-Path $gitPath
+    $parentDir = $parentDir.Parent
+  }
+  $isGitDir
+}
+
+# Check if git is installed
 try {
   Get-Command git.exe -ErrorAction Stop | Out-Null
   $gitInstalled = $true
@@ -99,21 +108,20 @@ catch {
 function prompt { 
   Write-Host "" 
   Write-Host ("[" + ($(Get-Date).toString("MM/dd/yyyy hh:mm:ss")) + "] [" + $(Get-Location).path + "]") -NoNewline
-  
   # Check if we're in a git repo
-  if ($gitInstalled -and (Test-Path $(Join-Path $pwd ".git"))) {
+  if ($gitInstalled -and (Test-GitRepo)) {
     $output = &git status
     $branch = $output[0].Replace('On branch ', '')
-    if ($output[1] -like '*clean') {
+    if ($output[3] -like '*clean') {
       Write-Host -NoNewline -ForegroundColor Green " [$branch]"
     }
     else {
       Write-Host -NoNewLine -ForegroundColor Red " [$branch]"
     }
   }
-  Write-Host ""
-  
+
   $prompt_text = "PS>"
+  Write-Host ""
   if (Test-Administrator) {
     Write-Host -ForegroundColor Red "[ADMIN]" -NoNewline
     $prompt_text = " PS#"
